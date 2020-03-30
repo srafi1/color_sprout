95fc33c7901bbed59a9e862053e7178e2fe08fe7
srafi1 <shakilrafi0@gmail.com>
2020-03-29 21:23:18 -0400

    main menu

--- lib/game_controller.dart
+++ lib/game_controller.dart
@@ -1,11 +1,89 @@
 import 'package:color_sprout/components/background.dart';
 import 'package:flame/game.dart';
+import 'package:flame/text_config.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class GameController extends BaseGame {
-    SharedPreferences storage;
+class GameController extends BaseGame with HasWidgetsOverlay {
+  SharedPreferences storage;
 
-    GameController() {
-        add(BackgroundComponent());
-    }
+  TextConfig regularText;
+  TextStyle textStyle;
+
+  GameController() {
+    regularText = TextConfig(color: Colors.black);
+    textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 40,
+    );
+
+    add(BackgroundComponent());
+
+    addWidgetOverlay(
+      "mainMenu",
+      buildMainMenu()
+    );
+  }
+
+  Widget buildIconButton({Color color, Icon icon, Function callback, double size: 50}) {
+    return 
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: ClipOval(
+          child: Container(
+            width: size,
+            height: size,
+            color: color,
+            child: IconButton(
+              icon: icon,
+              iconSize: size / 2,
+              color: Colors.white,
+              onPressed: callback,
+            ),
+          )
+        ),
+      );
+  }
+
+  Widget buildMainMenu() {
+    return 
+      Card(
+        margin: EdgeInsets.only(),
+        color: Color(0xFFF3F3F3),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Color Sprout", style: textStyle),
+              buildIconButton(
+                icon: Icon(Icons.play_arrow),
+                color: Colors.blue,
+                callback: () { print("Clicked play"); },
+                size: 100,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildIconButton(
+                    icon: Icon(Icons.settings),
+                    color: Colors.orange,
+                    callback: () { print("Clicked settings"); },
+                  ),
+                  buildIconButton(
+                    icon: Icon(Icons.list),
+                    color: Colors.green,
+                    callback: () { print("Clicked list"); },
+                  ),
+                  buildIconButton(
+                    icon: Icon(Icons.favorite),
+                    color: Colors.red,
+                    callback: () { print("Clicked favorite"); },
+                  ),
+                ],
+              )
+            ]
+          )
+        )
+      );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:color_sprout/game_controller.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-    GameController gc = GameController();
-    runApp(gc.widget);
+  GameController gc = GameController();
+  runApp(gc.widget);
 
-    Util flameUtil = Util();
-    await flameUtil.fullScreen();
-    await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+  SharedPreferences storage = await SharedPreferences.getInstance();
+  gc.storage = storage;
+
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 }
